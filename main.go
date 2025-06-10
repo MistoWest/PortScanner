42335ccc366a6db1f7961f80d5cc471a35baff3b
Wesley H. - MrWest <65507985+MistoWest@users.noreply.github.com>
2025-06-09 23:30:10 -0300

    Update main.go

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"sort"
 )
 
 const (
-	MAX_PORTS = 65535
+	MAX_PORTS uint32 = 99999
 )
 
-func worker(ports, results chan int) {
+// https://github.com/thanhphuchuynh/tcp-scanner-go-basic/blob/main/main.go
+func worker(ports, results chan int, site string) {
 	for p := range ports {
-		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		address := fmt.Sprintf("%s:%d", site, p)
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
 			results <- 0
@@ -24,21 +26,42 @@ func worker(ports, results chan int) {
 }
 
 func main() {
-	ports := make(chan int, MAX_PORTS)
-	results := make(chan int)
-	var open_ports []int
+	fmt.Println("Port Scanner - MistoWest")
+	fmt.Println("-----------------------------------------")
 
+	var open_ports []int
+	var qnt_ports uint32
+	var site string
+
+	fmt.Printf("Digite o site: ")
+	fmt.Scan(&site)
+
+	fmt.Printf("Quantidade de portas (Máximo - %d): ", MAX_PORTS)
+	fmt.Scan(&qnt_ports)
+
+	if qnt_ports > MAX_PORTS {
+		log.Fatal("Quantidade de portas maior que o limite")
+	}
+
+	if qnt_ports <= 0 {
+		log.Fatal("Quantidade de portas menor ou igual a 0")
+	}
+
+	ports := make(chan int, qnt_ports)
+	results := make(chan int)
+
+	fmt.Printf("Iniciando escaneamento...\n")
 	for i := 0; i < cap(ports); i++ {
-		go worker(ports, results)
+		go worker(ports, results, site)
 	}
 
 	go func() {
-		for i := 1; i <= MAX_PORTS; i++ {
+		for i := 1; i <= int(MAX_PORTS); i++ {
 			ports <- i
 		}
 	}()
 
-	for i := 0; i < MAX_PORTS; i++ {
+	for i := 0; i < int(MAX_PORTS); i++ {
 		port := <-results
 		if port != 0 {
 			open_ports = append(open_ports, port)
@@ -49,6 +72,9 @@ func main() {
 	close(results)
 
 	sort.Ints(open_ports)
+	fmt.Println("Site: ", site)
+	fmt.Printf("Quantidade de portas abertas: %d\n", len(open_ports))
+	fmt.Println("-----------------------------------------")
 	for _, port := range open_ports {
 		fmt.Printf("%d open\n", port)
 	}
